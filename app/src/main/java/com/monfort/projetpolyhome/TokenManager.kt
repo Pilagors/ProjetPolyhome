2abb202b44875502012ae45c214f74f70642e2b9
Lucas Monfort <Lucas_Monfort@etu.ube.fr>
2026-03-14 11:48:51 +0100

    Ajout d'un Token Manager

---
+++ app/src/main/java/com/monfort/projetpolyhome/TokenManager.kt
@@ -0,0 +1,26 @@
+package com.monfort.projetpolyhome
+
+import android.annotation.SuppressLint
+import android.content.Context
+
+class TokenManager(context: Context) {
+    private val prefs = context.getSharedPreferences("auth", Context.MODE_PRIVATE)
+
+    companion object {
+        private const val TOKEN_KEY = "token"
+    }
+
+    @SuppressLint("UseKtx")
+    fun saveToken(token: String) {
+        prefs.edit().putString(TOKEN_KEY, token).apply()
+    }
+
+    fun getToken(): String? {
+        return prefs.getString(TOKEN_KEY, null)
+    }
+
+    fun isLogged(): Boolean {
+        return getToken() != null
+    }
+
+}
