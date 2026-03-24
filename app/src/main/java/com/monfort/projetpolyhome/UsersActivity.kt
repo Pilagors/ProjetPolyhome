6cead6d5cd528cacdf4a6da50754d6598c9e8dfd
Berthier <gabin@MacBook-Air-de-Berthier.local>
2026-03-24 14:40:12 +0100

    ajout list des users finis

--- app/src/main/java/com/monfort/projetpolyhome/UsersActivity.kt
+++ app/src/main/java/com/monfort/projetpolyhome/UsersActivity.kt
@@ -13,12 +13,13 @@ import com.monfort.projetpolyhome.adapters.UserAdapter
 import com.monfort.projetpolyhome.data.HouseData
 import com.monfort.projetpolyhome.data.UserData
 import com.monfort.projetpolyhome.utils.Api
+import com.monfort.projetpolyhome.utils.HouseIdManager
 import com.monfort.projetpolyhome.utils.TokenManager
 
 class UsersActivity : AppCompatActivity() {
     val usersInfos : ArrayList<UserData> = ArrayList()
     val housesInfos : ArrayList<HouseData> = ArrayList()
-    val myhouse : Int = -1
+    var myhouse : Int = -1
 
     lateinit var adapter: UserAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,8 +33,8 @@ class UsersActivity : AppCompatActivity() {
         }
 
         adapter = UserAdapter(this,usersInfos)
+        getmyHouse()
         initButton()
-        fetchmyHouse()
         fetchUsers()
         initListUsers()
     }
@@ -52,29 +53,9 @@ class UsersActivity : AppCompatActivity() {
         findViewById<ListView>(R.id.listUsers).adapter = adapter
     }
 
-    private fun fetchmyHouse () {
-        val token = TokenManager(this).getToken()
-
-        Api().get<List<HouseData>>("https://polyhome.lesmoulinsdudev.com/api/houses", ::successFetchHouses, token)
+    private fun getmyHouse(){
+        myhouse = HouseIdManager(this).getHouseId()!!
     }
-    private fun successFetchHouses(responseCode : Int, response : List<HouseData>?) {
-        runOnUiThread {
-            when(responseCode) {
-                200 -> {
-                    if (!response.isNullOrEmpty()) {
-                        housesInfos.addAll(response)
-                    }
-                }
-                403 -> {
-                    Toast.makeText(this,"Accès refusé",Toast.LENGTH_SHORT).show()
-                }
-                500 -> {
-                    Toast.makeText(this,"erreur serveur",Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
-    }
-
 
     private fun fetchUsers(){
         val token = TokenManager(this).getToken()
